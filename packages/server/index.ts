8a295061495a8a9b30f05b487f9b16ff7e9aab1e
megagig <megagigsoftwaresolutions@gmail.com>
2025-09-02 20:16:36 +0100

    Create the Frontend

---
+++ packages/server/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+
+import type { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+// Set port from environment variable OR default to 3000
+const port = process.env.PORT || 3000;
+
+// Define a basic route
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`🚀 Server is running on http://localhost:${port}`);
+});
